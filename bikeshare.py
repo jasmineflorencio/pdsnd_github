11b966584a5fb984fa525149e81b5f3e1b33e6ab
Jasmine Florencio <jasmineflorencio@gmail.com>
2020-08-31 13:12:12 -0700

    update user_stats docstring to include function arguments

--- bikeshare.py
+++ bikeshare.py
@@ -140,7 +140,12 @@ def trip_duration_stats(df):
     print('-'*40)
 
 def user_stats(df, city):
-    """Displays statistics on bikeshare users."""
+    """
+    Displays statistics on bikeshare users.
+
+    Args:
+        (str) city - name of the city to analyze
+    """
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
